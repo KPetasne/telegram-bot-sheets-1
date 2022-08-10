fd7263e53b2503026b8f2e8eed8862498952dced
Kevin Petasne <kevinpetasne@Kevins-MBP.fibertel.com.ar>
2022-08-10 20:13:04 -0300

    Add rate limit

--- src/app/bot/bot.ts
+++ src/app/bot/bot.ts
@@ -1,11 +1,20 @@
 import { Scenes, session, Telegraf, Markup } from 'telegraf';
+import { rateLimit } from 'telegraf-ratelimit'
 import { gasto } from './scenes/gasto'
 import { aporte } from './scenes/aporte'
+ 
+// Set limit to 1 message per 3 seconds
+const limitConfig = {
+  window: 3000,
+  limit: 1,
+  onLimitExceeded: (ctx, next) => ctx.reply('Rate limit exceeded')
+}
 
 const bot: Telegraf<Scenes.WizardContext> = new Telegraf(process.env.BOT_TOKEN as string);
 const stage = new Scenes.Stage<Scenes.WizardContext>([aporte, gasto]);
 
 bot.use(session());
+bot.use(rateLimit(limitConfig))
 bot.use(stage.middleware())
 bot.command('aporte', async (ctx) => {
     await ctx.scene.enter('aporte');
