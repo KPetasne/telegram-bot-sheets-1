e0cff249b4086895f7d58d94af258849a26e4bbd
Kevin Petasne <kevinpetasne@Kevins-MacBook-Pro.local>
2024-01-03 20:16:28 -0300

    netlify test 2

--- netlify/functions/app.ts
+++ netlify/functions/app.ts
@@ -1,6 +1,17 @@
 import { bot } from './app/bot/bot'
 
+
 bot.launch();
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+exports.handler = async event => {
+    try {
+      await bot.handleUpdate(JSON.parse(event.body))
+      return { statusCode: 200, body: "" }
+    } catch (e) {
+      console.error("error in handler:", e)
+      return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
+    }
+  }
